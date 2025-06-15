69c9e01316f618dab6bfeb93d0efbc68f69fba6e
YiRanMushroom <yiranmushroom@gmail.com>
2025-06-15 04:59:54 -0700

    Fix color problems.

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,6 +10,65 @@ bool QueueFamilyIndices::IsComplete() const {
     return GraphicsFamily.has_value() && PresentFamily.has_value();
 }
 
+inline void ImGuiUseStyleColorHazel() {
+    auto &colors = ImGui::GetStyle().Colors;
+    colors[ImGuiCol_WindowBg] = ImVec4{0.1f, 0.105f, 0.11f, 1.0f};
+
+    // Headers
+    colors[ImGuiCol_Header] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+    colors[ImGuiCol_HeaderHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
+    colors[ImGuiCol_HeaderActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+
+    // Buttons
+    colors[ImGuiCol_Button] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+    colors[ImGuiCol_ButtonHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
+    colors[ImGuiCol_ButtonActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+
+    // Frame BG
+    colors[ImGuiCol_FrameBg] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
+    colors[ImGuiCol_FrameBgHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
+    colors[ImGuiCol_FrameBgActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+
+    // Tabs
+    // colors[ImGuiCol_Tab] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+    // colors[ImGuiCol_TabHovered] = ImVec4{0.38f, 0.3805f, 0.381f, 1.0f};
+    colors[ImGuiCol_Tab] = ImVec4(0.18f, 0.185f, 0.19f, 1.00f);
+    colors[ImGuiCol_TabHovered] = ImVec4(0.28f, 0.285f, 0.29f, 1.00f);
+    colors[ImGuiCol_TabSelected] = ImVec4(0.22f, 0.225f, 0.23f, 1.00f);
+    colors[ImGuiCol_TabSelectedOverline] = ImVec4(0.35f, 0.40f, 0.60f, 1.00f);
+    colors[ImGuiCol_TabDimmed] = ImVec4(0.13f, 0.135f, 0.14f, 1.00f);
+    colors[ImGuiCol_TabDimmedSelected] = ImVec4(0.18f, 0.185f, 0.19f, 1.00f);
+    colors[ImGuiCol_TabDimmedSelectedOverline] = ImVec4(0.25f, 0.30f, 0.45f, 1.00f);
+
+    // Title
+    colors[ImGuiCol_TitleBg] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+    colors[ImGuiCol_TitleBgActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+    colors[ImGuiCol_TitleBgCollapsed] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
+
+    // Resize Grip
+    colors[ImGuiCol_ResizeGrip] = ImVec4(0.91f, 0.91f, 0.91f, 0.25f);
+    colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.81f, 0.81f, 0.81f, 0.67f);
+    colors[ImGuiCol_ResizeGripActive] = ImVec4(0.46f, 0.46f, 0.46f, 0.95f);
+
+    // Scrollbar
+    colors[ImGuiCol_ScrollbarBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
+    colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.31f, 0.31f, 0.31f, 1.0f);
+    colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.41f, 0.41f, 0.41f, 1.0f);
+    colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.51f, 0.51f, 0.51f, 1.0f);
+
+    // Check Mark
+    colors[ImGuiCol_CheckMark] = ImVec4(0.94f, 0.94f, 0.94f, 1.0f);
+
+    // Slider
+    colors[ImGuiCol_SliderGrab] = ImVec4(0.51f, 0.51f, 0.51f, 0.7f);
+    colors[ImGuiCol_SliderGrabActive] = ImVec4(0.66f, 0.66f, 0.66f, 1.0f);
+
+    // Docking
+    // colors[ImGuiCol_DockingPreview] = ImVec4(0.31f, 0.31f, 0.31f, 0.7f);
+    // colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.02f, 0.02f, 0.02f, 1.0f);
+}
+
+
 VKAPI_ATTR vk::Bool32 VKAPI_CALL DebugCallback(
     vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
     vk::DebugUtilsMessageTypeFlagsEXT messageType,
@@ -248,7 +307,7 @@ SwapChainSupportDetails HelloTriangleApplication::QuerySwapChainSupport(vk::Phys
 vk::SurfaceFormatKHR HelloTriangleApplication::ChooseSwapSurfaceFormat(
     const std::vector<vk::SurfaceFormatKHR> &availableFormats) const {
     for (const auto &availableFormat: availableFormats) {
-        if (availableFormat.format == vk::Format::eB8G8R8A8Srgb &&
+        if (availableFormat.format == vk::Format::eB8G8R8A8Unorm &&
             availableFormat.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
             return availableFormat;
         }
@@ -379,8 +438,9 @@ void HelloTriangleApplication::InitImGui() {
     //io.ConfigViewportsNoAutoMerge = true;
     //io.ConfigViewportsNoTaskBarIcon = true;
 
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
+    // ImGui::StyleColorsDark();
+    ImGuiUseStyleColorHazel();
+    // ImGui::StyleColorsLight();
 
     // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
     ImGuiStyle &style = ImGui::GetStyle();
@@ -706,17 +766,11 @@ void HelloTriangleApplication::CreateSyncObjects() {
         m_ImageAvailableSemaphores.push_back(m_Device.createSemaphore(semaphoreInfo).value());
         m_InFlightFences.push_back(m_Device.createFence(fenceInfo).value());
     }
-    // m_ImageAvailableSemaphore = m_Device.createSemaphore(semaphoreInfo).value();
-    // m_RenderFinishedSemaphore = m_Device.createSemaphore(semaphoreInfo).value();
-
-
 
     // m_InFlightFence = m_Device.createFence(fenceInfo).value();
     for (size_t i = 0; i < m_SwapChainImages.size(); i++) {
         m_RenderFinishedSemaphores.push_back(m_Device.createSemaphore(semaphoreInfo).value());
     }
-
-    // m_ImagesInFlight.resize(m_SwapChainImages.size(), nullptr);
 }
 
 void HelloTriangleApplication::RecordCommandBuffer(vk::CommandBuffer commandBuffer, uint32_t imageIndex) {
@@ -733,7 +787,7 @@ void HelloTriangleApplication::RecordCommandBuffer(vk::CommandBuffer commandBuff
     }
 
     vk::ClearValue clearColor{
-        vk::ClearColorValue(std::array<float, 4>{.5f, .0f, .5f, 1.0f})
+        vk::ClearColorValue(std::array<float, 4>{.80f, .10f, .80f, 1.0f})
     };
 
     vk::RenderPassBeginInfo renderPassInfo{
